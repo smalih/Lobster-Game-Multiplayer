3dbe98c58238582b0644f176aaf78a255e2ed35b
Solomon Malih <smalih15@hotmail.com>
2022-02-08 21:06:18 +0000

    added option for player to join on home screen using name and known game id

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,11 @@
 $("#btnCreate").click(function () {
   window.location.href = `http://localhost:8080/game/new`;
 });
+
+$("#btnJoin").click(function () {
+  if ($("#gameId").val() !== "" && $("#playerName").val() !== "") {
+    gameId = $("#gameId").val();
+    playerName = $("#playerName").val();
+    window.location.href = `http://localhost:8080/game/${gameId}?name=${playerName}`;
+  }
+});
